812ae315ad8c39ace6fa74f1f239ebb754add671
孙苗苗 <cg_smm@163.com>
2016-07-25 11:32:48 +0800

    160725好好学习

---
+++ 160725.js
@@ -0,0 +1,10 @@
+/*
+* @Author: Administrator
+* @Date:   2016-07-25 11:29:47
+* @Last Modified by:   Administrator
+* @Last Modified time: 2016-07-25 11:30:12
+*/
+
+'use strict';
+
+cosole.log('今天天气不错，哈哈哈');
