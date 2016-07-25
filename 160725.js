800d02c4c9407b83d73fad5932e3a3b3ca116766
孙苗苗 <cg_smm@163.com>
2016-07-25 13:13:18 +0800

    ll说

--- 160725.js
+++ 160725.js
@@ -2,7 +2,7 @@
 * @Author: Administrator
 * @Date:   2016-07-25 11:29:47
 * @Last Modified by:   Administrator
-* @Last Modified time: 2016-07-25 12:44:21
+* @Last Modified time: 2016-07-25 13:12:40
 */
 
 'use strict';
@@ -10,3 +10,5 @@
 cosole.log('今天天气不错，哈哈哈');
 
 cosole.log('今天天气不错，吃了好多好吃的，哈哈哈哈');
+cosole.log('这个是ll的文件');
+console.log('ll说：pp是正方形');
