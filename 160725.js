4afbbae3c49c61059cfcf5c1a0ff27c775cebea7
孙苗苗 <cg_smm@163.com>
2016-07-25 12:45:05 +0800

    160725吃吃吃吃

--- 160725.js
+++ 160725.js
@@ -2,9 +2,11 @@
 * @Author: Administrator
 * @Date:   2016-07-25 11:29:47
 * @Last Modified by:   Administrator
-* @Last Modified time: 2016-07-25 11:30:12
+* @Last Modified time: 2016-07-25 12:44:21
 */
 
 'use strict';
 
 cosole.log('今天天气不错，哈哈哈');
+
+cosole.log('今天天气不错，吃了好多好吃的，哈哈哈哈');
